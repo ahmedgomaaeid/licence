3b30b7b16bb5f3c38d48452547004aa1dbc33964
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-09-23 02:08:11 +0300

    database

--- database/migrations/2023_09_22_221901_create_users_table.php
+++ database/migrations/2023_09_22_221901_create_users_table.php
@@ -14,10 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('nId');
+            $table->integer('phone');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('operation_id')->constrained('operations')->onDelete('cascade');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_09_22_230337_create_forms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('type');
+            $table->integer('reson');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('nationality');
+            $table->integer('job_number');
+            $table->string('job');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('operation_admin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forms');
+    }
+};
